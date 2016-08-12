954248c6af02826774cb57f6f89e9ce59057e0cf
marinacor1 <marinaspam1234@gmail.com>
2016-08-11 22:25:59 -0600

    No trailing space allowed in string

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -4,18 +4,21 @@ Words.prototype.count = function (input){
   var input = input.replace(/\n/, ' ');
   var input = input.replace(/\t/, ' ');
   var input = input.replace('  ', ' ');
+  var input = input.trim();
   var eachWord = input.split(" ");
+
   var groupedWords = new Object();
-  var counter = 0;
   for (var i = 0; i < eachWord.length; i ++){
+
     var word = eachWord[i].toLowerCase();
-    var counter = counter || 0;
-    if (Object.keys(groupedWords).indexOf(word) > -1){
-      groupedWords[word] = groupedWords[word] + 1;
-    }
-    else {
-      groupedWords[word] = 1;
-    }
+    var counter = 0;
+      if (Object.keys(groupedWords).indexOf(word) > -1){
+        groupedWords[word] = groupedWords[word] + 1;
+      }
+      else {
+        groupedWords[word] = 1;
+      }
+
   }
   return groupedWords;
 };
