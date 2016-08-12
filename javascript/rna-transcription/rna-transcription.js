2264777e6eddc8835ca5bfb5b205b1eb08fb3e8b
marinacor1 <marinaspam1234@gmail.com>
2016-08-11 21:18:36 -0600

    All tests passing

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,18 +1,40 @@
 var DnaTranscriber = function(){};
 
 DnaTranscriber.prototype.toRna = function (input){
-  if (input === 'G'){
-    return 'C';
-  }
-  else if (input === 'C'){
-    return 'G';
-  }
-  else if (input === 'T'){
-    return 'A';
+  if (input.length === 1){
+    if (input === 'G'){
+      return 'C';
+    }
+    else if (input === 'C'){
+      return 'G';
+    }
+    else if (input === 'T'){
+      return 'A';
+    }
+    else {
+      return 'U';
+    }
   }
   else {
-    return 'U';
+    var inputs = input.split('');
+    var finalRna = [];
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i] === 'G'){
+        finalRna[i] = 'C';
+      }
+      else if (inputs[i] === 'C'){
+        finalRna[i] = 'G';
+      }
+      else if (inputs[i] === 'T'){
+        finalRna[i] =  'A';
+      }
+      else if (inputs[i] === 'A'){
+        finalRna[i] = 'U';
+      }
+     var joinedRna = finalRna.join();
+    }
   }
+  return joinedRna.replace(/\,/g, '');
 };
 
 
