15e40e86e8fc72dccb9f787d0f961c8c893a6301
marinacor1 <marinaspam1234@gmail.com>
2016-08-24 16:05:43 -0600

    Refactor works

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -3,12 +3,20 @@ var Anagram = function (focusWord){
 };
 
 Anagram.prototype.matches = function (list) {
+  if (arguments[0].constructor === Array){
+    result = checkArray(list, this.focusWord);
+  }
+  return result;
+};
+
+function checkArray(list, x_this){
+  console.log(x_this);
   var results = [];
   for (m = 0; m < list.length; m++){
     var word = list[m].split('');
     var value = true;
     for (i = 0; i < word.length; i++){
-      var keyWord = this.focusWord.toLowerCase();
+      var keyWord = x_this.toLowerCase();
       var letter = word[i].toLowerCase();
       if (!keyWord.includes(letter)) {
         value = false;
@@ -17,12 +25,11 @@ Anagram.prototype.matches = function (list) {
         value = false;
       }
     }
-    if (this.focusWord.length == word.length && value === true){
+    if (x_this.length == word.length && value === true){
         results.push(word.join(''));
       }
     }
     return results;
-};
 
-
+}
 module.exports = Anagram;
